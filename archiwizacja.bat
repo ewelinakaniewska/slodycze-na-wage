044fe202cea346f9acbeb1fcf4858d8263807bbc
student <ek125129@ur.edu.pl>
2026-05-05 18:41:47 +0200

    usuniecie .bat

--- archiwizacja.bat
+++
@@ -1,7 +0,0 @@
-git init
-git config --local user.name "student"
-git config --local user.email "ek125129@ur.edu.pl"
-git add --all
-git commit -m "Projekt"
-git archive --format=zip HEAD -o ../125129_AI1_projekt.zip
-pause
