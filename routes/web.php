11df4b2e4271613f34e3a287186dcbcf39526a4b
Ewelina Kaniewska <ekaniewska144@gmail.com>
2026-05-10 13:56:02 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,50 +10,34 @@ use App\Http\Middleware\IsAdmin;
 use App\Http\Middleware\Auth;
 
 
+Route::get('/', [CandyController::class, 'index'])->name('candies.index');
 
-Route::get('/', function () {
-    return "LOG: Strona glowna dziala bez middleware. Jesli to widzisz, winny jest CandyController lub Middleware.";
+Route::get('/candies', [CandyController::class, 'index']);
+Route::get('/candies/{candy}', [CandyController::class, 'show'])->name('candies.show');
+Route::get('/suppliers', [SupplierController::class, 'index'])->name('suppliers.index');
+
+Route::get('/faq', function () {
+    return view('faq');
+})->name('faq');
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/auth/login', 'login')->name('login');
+    Route::post('/auth/login', 'authenticate')->name('login.authenticate');
+    Route::get('/auth/logout', 'logout')->name('logout');
+    Route::get('/auth/register', 'registerView')->name('register');
+    Route::post('/auth/register', 'register')->name('register.register');
 });
 
-Route::get('/debug-session', function () {
-    return response()->json([
-        'session_id' => session()->getId(),
-        'user' => auth()->user(),
-        'url' => config('app.url'),
-    ]);
+Route::middleware([IsAdmin::class])->group(function () {
+    Route::get('/users-list', [UserController::class, 'index'])->name('users.index');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+    Route::resource('suppliers', SupplierController::class)->except(['index']);
+
+    Route::resource('candies', CandyController::class)->except(['index', 'show']);
 });
 
-
-
-
-// Route::get('/', [CandyController::class, 'index'])->name('candies.index');
-
-// Route::get('/candies', [CandyController::class, 'index']);
-// Route::get('/candies/{candy}', [CandyController::class, 'show'])->name('candies.show');
-// Route::get('/suppliers', [SupplierController::class, 'index'])->name('suppliers.index');
-
-// Route::get('/faq', function () {
-//     return view('faq');
-// })->name('faq');
-
-// Route::controller(AuthController::class)->group(function () {
-//     Route::get('/auth/login', 'login')->name('login');
-//     Route::post('/auth/login', 'authenticate')->name('login.authenticate');
-//     Route::get('/auth/logout', 'logout')->name('logout');
-//     Route::get('/auth/register', 'registerView')->name('register');
-//     Route::post('/auth/register', 'register')->name('register.register');
-// });
-
-// Route::middleware([IsAdmin::class])->group(function () {
-//     Route::get('/users-list', [UserController::class, 'index'])->name('users.index');
-//     Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-
-//     Route::resource('suppliers', SupplierController::class)->except(['index']);
-
-//     Route::resource('candies', CandyController::class)->except(['index', 'show']);
-// });
-
-// Route::middleware([Auth::class])->group(function () {
-//     Route::resource('orders', OrderController::class);
-//     Route::resource('users', UserController::class)->except(['index']);
-// });
+Route::middleware([Auth::class])->group(function () {
+    Route::resource('orders', OrderController::class);
+    Route::resource('users', UserController::class)->except(['index']);
+});
